c468c30981d13c553f959cd2d466afdc9a4cffdf
key262yek <key262yek@gmail.com>
2022-08-03 15:03:34 +0900

    Implement SpeedCam

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
 use moldybrody::prelude::*;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Serialize, Deserialize, State)]
+#[derive(Debug, Clone)]
+pub enum TrafficItem<'a> {
+    Car(Car),
+    Flag(SpeedCamFlag<'a>)
+}
+
+impl<'a> TrafficItem<'a> {
+    fn pos(&self) -> &Cartessian1D<f64> {
+        match self{
+            TrafficItem::Car(c) => &c.pos,
+            TrafficItem::Flag(f) => &f.pos
+        }
+    }
+
+    fn set_max_speed(&self, other : &mut Self) {
+        match (self, other) {
+            (TrafficItem::Flag(f), TrafficItem::Car(c)) => {
+                f.set_max_speed(c);
+            }
+            _ => unreachable!(),
+        }
+    }
+}
+
+#[derive(Debug, Serialize, Deserialize, State, Clone)]
 pub struct Car {
     pos: Cartessian1D<f64>,
     vel: Cartessian1D<f64>,
@@ -11,10 +35,11 @@ pub struct Car {
     drift: f64,
     mass: f64,
     behavior: f64,
+    own_max_speed : f64,
 }
 
 impl Car {
-    pub fn new(lane: usize, size: f64, max_speed: f64, drift: f64, behavior: f64) -> Self {
+    pub fn new(lane: usize, size: f64, max_speed: f64, drift: f64, behavior: f64, own_max_speed: f64) -> Self {
         Self {
             pos: Cartessian1D { coord: [0f64] },
             vel: Cartessian1D { coord: [0f64] },
@@ -24,6 +49,7 @@ impl Car {
             drift,
             mass: 1f64,
             behavior,
+            own_max_speed,
         }
     }
 
@@ -60,14 +86,18 @@ impl Car {
     }
 }
 
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
 pub struct SpeedCam {
     pos: Cartessian1D<f64>,
     speed_limit: f64,
+    length : f64,
+    check_average : bool,
 }
 
 impl SpeedCam {
-    pub fn new(pos: Cartessian1D<f64>, speed_limit: f64) -> Self {
-        Self { pos, speed_limit }
+    pub fn new(pos: Cartessian1D<f64>, speed_limit: f64, length: f64, check_average: bool) -> Self {
+        Self { pos, speed_limit, length, check_average }
     }
 
     pub fn set_max_speed(&self, car: &mut Car) {
@@ -78,10 +108,43 @@ impl SpeedCam {
         if car.vel[0] < self.speed_limit {
             Cartessian1D::zeros()
         } else {
-            Cartessian1D::zeros()
+            Cartessian1D::new([- 2.0 / self.length * car.vel[0].powi(2)])
         }
     }
+
+    pub fn flags<'a>(&'a self) -> (SpeedCamFlag<'a>, SpeedCamFlag<'a>){
+        (SpeedCamFlag::new(&self.pos - Cartessian1D::new([self.length]), true, &self),
+        SpeedCamFlag::new(self.pos.clone(), false, &self))
+    }
 }
+
+
+#[derive(Debug, Clone, Serialize)]
+pub struct SpeedCamFlag<'a> {
+    pos: Cartessian1D<f64>,
+    status : bool,
+    cam : &'a SpeedCam,
+}
+
+impl<'a> SpeedCamFlag<'a> {
+    pub fn new(pos : Cartessian1D<f64>, status : bool, cam : &'a SpeedCam) -> Self{
+        Self{ pos, status, cam }
+    }
+
+    pub fn set_max_speed(&self, other : &mut Car) {
+        if self.status {
+            self.cam.set_max_speed(other);
+        } else {
+            other.max_speed = other.own_max_speed;
+        }
+    }
+
+    pub fn pos(&self) -> &Cartessian1D<f64> {
+        &self.pos
+    }
+}
+
+
 
 #[cfg(test)]
 mod tests {
@@ -92,7 +155,7 @@ mod tests {
     fn test_car_acceleration() {
         let mut timeiter = ConstStep::<f64>::new(1e-1).unwrap();
         timeiter.set_tmax(20f64).unwrap();
-        let mut car = Car::new(0, 1f64, 10f64, 1f64, 0f64);
+        let mut car = Car::new(0, 1f64, 10f64, 1f64, 0f64, 10f64);
 
         for (_t, dt) in timeiter.into_diff() {
             let force = car.drift_force();
@@ -112,7 +175,7 @@ mod tests {
         );
         let mut timeiter = ConstStep::<f64>::new(1e-1).unwrap();
         timeiter.set_tmax(40f64).unwrap();
-        let mut car = Car::new(0, 1f64, 10f64, 1f64, 0f64);
+        let mut car = Car::new(0, 1f64, 10f64, 1f64, 0f64, 10f64);
 
         for (_t, dt) in timeiter.into_diff() {
             let force = car.drift_force();
@@ -128,8 +191,8 @@ mod tests {
         let mut timeiter = ConstStep::<f64>::new(1e-1).unwrap();
         timeiter.set_tmax(100f64).unwrap();
         let mut cars = [
-            Car::new(0, 1f64, 10f64, 1f64, 0f64),
-            Car::new(0, 1f64, 50f64, 1f64, 0f64),
+            Car::new(0, 1f64, 10f64, 1f64, 0f64, 10f64),
+            Car::new(0, 1f64, 50f64, 1f64, 0f64, 10f64),
         ];
 
         cars[0].pos[0] = 50f64;
@@ -152,5 +215,47 @@ mod tests {
         }
 
         assert!(cars[1].vel[0] < 10f64);
+    }
+
+    #[test]
+    fn test_speedcam() {
+        let mut timeiter = ConstStep::<f64>::new(1e-1).unwrap();
+        timeiter.set_tmax(100f64).unwrap();
+        let speedcam = SpeedCam::new(Cartessian1D::new([500f64]), 5f64, 100f64, false);
+        let (open, close) = speedcam.flags();
+
+        let mut items : Vec<TrafficItem> = vec![
+            TrafficItem::Car(Car::new(0, 1f64, 10f64, 1f64, 0f64, 10f64)),
+            TrafficItem::Flag(open),
+            TrafficItem::Flag(close),
+        ];
+
+        for (_t, dt) in timeiter.into_diff() {
+            for item in items.iter_mut(){
+                match item {
+                    TrafficItem::Car(c) => {
+                        let force = c.drift_force();
+                        let movement = c.euler(&force, dt);
+                        c.renew_state(&movement);
+                    },
+                    TrafficItem::Flag(_) => {},
+                }
+            }
+
+            for i in 1..items.len() {
+                if let TrafficItem::Flag(f) = items[i].clone(){
+                    if let TrafficItem::Car(mut c) = items[i - 1].clone(){
+                        if f.pos() < c.pos(){
+                            f.set_max_speed(&mut c);
+                            if !f.status{
+                                assert!(c.vel[0] < speedcam.speed_limit * 1.1);
+                            }
+                            items[i] = TrafficItem::Car(c);
+                            items[i - 1] = TrafficItem::Flag(f);
+                        }
+                    }
+                }
+            }
+        }
     }
 }
